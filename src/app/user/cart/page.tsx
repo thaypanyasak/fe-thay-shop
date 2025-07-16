ff68aee32420a95c78891fd731148d645d084566
Thay19 <59501651+thaypanyasak@users.noreply.github.com>
2025-07-17 04:17:56 +0700

    Update page.tsx

--- src/app/user/cart/page.tsx
+++ src/app/user/cart/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useCartStore } from "@/app/store/useCartStore";
 import { useUserStore } from "@/app/store/useUserStore";
 import axios from "axios";
